d8b763c7eaa0c1866df6789ecc2f970878a123cb
Stephan Altmann <stephan.altmann@AGAN-MAC020.fritz.box>
2022-11-06 15:30:14 +0100

    add demo behat test

--- config/bundles.php
+++ config/bundles.php
@@ -1,20 +1,19 @@
 <?php
 
-declare(strict_types=1);
-
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                 => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class     => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class                        => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                    => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class                  => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class                    => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class                        => ['dev' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class               => ['all' => true],
-    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class                 => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class         => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
